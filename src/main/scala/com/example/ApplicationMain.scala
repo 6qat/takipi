b2da81cadcfc177032a67b780c2316420db17cd2
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2016-08-01 10:17:43 -0300

    Correct readline code

--- src/main/scala/com/example/ApplicationMain.scala
+++ src/main/scala/com/example/ApplicationMain.scala
@@ -6,10 +6,21 @@ object ApplicationMain extends App {
   val system = ActorSystem("MyActorSystem")
   val pingActor = system.actorOf(PingActor.props, "pingActor")
   pingActor ! PingActor.Initialize
-  // This example app will ping pong 3 times and thereafter terminate the ActorSystem - 
-  // see counter logic in PingActor
 
-  scala.io.StdIn.readLine()
-  system.terminate()
-  system.awaitTermination()
+  readLine(after = () => system.terminate())
+
+  def readLine(action: (String) => Unit = (line) => println(line),
+               quit: List[String] = List(""),
+               after: () => Unit = () => {}
+              ): Unit = {
+
+    Iterator
+      .continually(scala.io.StdIn.readLine())
+      .takeWhile(line => !quit.contains(line))
+      .foreach(line => action(s"$line"))
+
+    after()
+
+  }
+
 }
